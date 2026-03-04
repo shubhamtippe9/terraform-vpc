89e8c33ca6fd8ad28897510643d521a0464b5c36
shubham <shubhamtippe9@gmail.com>
2026-03-04 15:34:52 +0530

    3rd commit

--- terraform.tf
+++ terraform.tf
@@ -33,6 +33,7 @@ resource "aws_default_route_table" "my-route-table" {
     default_route_table_id = aws_vpc.my-vpc.default_route_table_id
     route {
         cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.my-igw.id
     }
 }
 resource "aws_route_table_association" "my-route-table-association" {
