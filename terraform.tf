a8b8eef4533a2ddb6b6d55e861d8032c30b389e6
shubham <shubhamtippe9@gmail.com>
2026-03-04 23:35:51 +0530

    4th commit

--- terraform.tf
+++ terraform.tf
@@ -40,4 +40,29 @@ resource "aws_route_table_association" "my-route-table-association" {
     subnet_id = aws_subnet.my-subnet-1.id
     route_table_id = aws_default_route_table.my-route-table.id
 }
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+}
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.my-subnet.id
+  tags = {
+    Name = NAT-RT
+  }
+  depends_on = [aws_internet_gateway.igw]
+}
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "private_nat" {
+  route_table_id         = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.NAT-RT.id
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.my-subnet.id
+  route_table_id = aws_route_table.private_rt.id
+}
 
